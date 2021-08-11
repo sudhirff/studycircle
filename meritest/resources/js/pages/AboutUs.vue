54fabdd649790ba9a66f0183ba843f9f661ff57a
Pallavi Dighe <pals.ver@gmail.com>
2021-08-11 19:47:51 +0530

    Resolved table issue in about us.

--- meritest/resources/js/pages/AboutUs.vue
+++ meritest/resources/js/pages/AboutUs.vue
@@ -1,39 +1,32 @@
 <template>
 	<div>
-	<breadcrumb :pageTitle="'About Us'"></breadcrumb>
+		<breadcrumb :pageTitle="'About Us'"></breadcrumb>
 		<div class="container pt-60 pb-70">
 					
 			<div class="row">
-				<h3>&ldquo;The use of online assessment tools is giving teachers a more fine-grained understanding of individual students&rsquo; skills, and assisting them to determine the necessary next steps to enable them to achieve their own learning goals. We are seeing more effective differentiation in classrooms as a result.&rdquo;</h3>
+				<div class="col-sm-10 col-sm-offset-1 col-md-12 col-md-offset-0">
+					<h3>&ldquo;The use of online assessment tools is giving teachers a more fine-grained understanding of individual students&rsquo; skills, and assisting them to determine the necessary next steps to enable them to achieve their own learning goals. We are seeing more effective differentiation in classrooms as a result.&rdquo;</h3>
 
-				<p>&mdash;&nbsp;Susan Mann</p>
+					<p>&mdash;&nbsp;Susan Mann</p>
 
-				<p><strong>MERITEST </strong>is an online exam software service provider brought to you by <strong>MERITEST SOLUTIONS Pvt Ltd.</strong><br />
-				We have developed an innovative <strong>online examination solution</strong> for university, colleges, institutions and corporate to conduct examinations anywhere, anytime.</p>
+					<p><strong>MERITEST </strong>is an online exam software service provider brought to you by <strong>MERITEST SOLUTIONS Pvt Ltd.</strong><br />
+					We have developed an innovative <strong>online examination solution</strong> for university, colleges, institutions and corporate to conduct examinations anywhere, anytime.</p>
 
-				<p>Our examination process have very fast processing speed, which is secure and simple that saves your time and money.&nbsp;</p>
+					<p>Our examination process have very fast processing speed, which is secure and simple that saves your time and money.&nbsp;</p>
 
-				<p>Use this <strong>test maker</strong> to create <strong>career </strong>booster <strong>Competitive </strong><em><strong>Exam</strong></em> <em>Online Test</em> Series for <em><strong>Bank</strong></em><strong>, </strong><em><strong>SSC</strong></em><strong>, </strong><em><strong>Railways</strong></em><strong>, LIC, RBI, CTET, GATE, Defence, UPSC, LAW, MBA, NET, BBA </strong>&amp; other {{ currentYear}} <em>Exams.</em><br />
-				Use our <em><strong>online test generator</strong></em> which creates <strong>Engineering and Medical</strong> entrance exam like <strong>JEE Main, JEE Advanced, BITSAT, VITEEE, SRMJEEE, COMEDK, KIITEE, WBJEE, MHTCET, BCECE, NTA NEET, AIIMS, JIPMER, AFMC</strong> and many more for your convenience.</p>
+					<p>Use this <strong>test maker</strong> to create <strong>career </strong>booster <strong>Competitive </strong><em><strong>Exam</strong></em> <em>Online Test</em> Series for <em><strong>Bank</strong></em><strong>, </strong><em><strong>SSC</strong></em><strong>, </strong><em><strong>Railways</strong></em><strong>, LIC, RBI, CTET, GATE, Defence, UPSC, LAW, MBA, NET, BBA </strong>&amp; other {{ currentYear}} <em>Exams.</em><br />
+					Use our <em><strong>online test generator</strong></em> which creates <strong>Engineering and Medical</strong> entrance exam like <strong>JEE Main, JEE Advanced, BITSAT, VITEEE, SRMJEEE, COMEDK, KIITEE, WBJEE, MHTCET, BCECE, NTA NEET, AIIMS, JIPMER, AFMC</strong> and many more for your convenience.</p>
 
-				<p>We will manage the entire process of the examination platform as a service, including exam center supervision and candidate validation except administrative activities.</p>
+					<p>We will manage the entire process of the examination platform as a service, including exam center supervision and candidate validation except administrative activities.</p>
 
-				<p>The system can be used for all types of <strong>online assessment</strong> services like quizzes, tests, assessments, exams or get feedback from surveys and polls. <strong>MERITEST</strong> is delivered through a <strong>SaaS model</strong> where the application is provided as a service to customers across the internet platform.</p>
+					<p>The system can be used for all types of <strong>online assessment</strong> services like quizzes, tests, assessments, exams or get feedback from surveys and polls. <strong>MERITEST</strong> is delivered through a <strong>SaaS model</strong> where the application is provided as a service to customers across the internet platform.</p>
 
-				<p><strong>FEATURES THAT MAKE MERITEST STAND OUT</strong></p>
-
-				<table class="table">
-					<tbody>
-						<tr>
-							<th width="50%">
-								<h4><span style="color: #ff991f">Top 10 features from the Administrator&rsquo;s point of view</span></h4>
-							</th>
-							<th width="50%">
-								<h4><span style="color: #ff991f">Top 10 features from the User&rsquo;s point of view</span></h4>
-							</th>
-						</tr>
-						<tr>
-							<td>
+					<p><strong>FEATURES THAT MAKE MERITEST STAND OUT</strong></p>
+					<br />
+					<div class="row">
+						<div class="col-md-6">
+							<div>
+								<h4><span class="org-color">Top 10 features from the Administrator&rsquo;s point of view</span></h4>
 								<ul class="list-group">
 									<li class="list-group-item">1. Analytical Report and Interactive Dashboard.</li>
 									<li class="list-group-item">2. Online quiz maker: Easy to add/import questions from word/excel format as per different topics &amp; subjects.</li>
@@ -46,8 +39,11 @@
 									<li class="list-group-item">9. Third-party integration is possible, i.e., SMS, email, online payment gateway etc.</li>
 									<li class="list-group-item">10. Sell your online test series and get monetary benefits. <router-link to="/features">...and many more</router-link></li>
 								</ul>
-							</td>
-							<td>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div>
+								<h4><span class="org-color">Top 10 features from the Administrator&rsquo;s point of view</span></h4>
 								<ul class="list-group">
 									<li class="list-group-item">1. Analytical Result View and Interactive Dashboard.</li>
 									<li class="list-group-item">2. Synchronization of online test on Web and Mobile/Tablet which is accessible 24/7.</li>
@@ -62,22 +58,22 @@
 										<router-link to="/features">...and many more</router-link>
 									</li>
 								</ul>
-							</td>
-						</tr>
-					</tbody>
-				</table>
+							</div>
+						</div>
+					</div>
 
-				<p>We will provide products as per your requirement <a href="/contact-us"><span style="color: #ff991f">(Contact Us)</span></a></p>
+					<p>We will provide products as per your requirement <a href="/contact-us"><span style="color: #ff991f">(Contact Us)</span></a></p>
 
-				<p><strong>MERITEST Exam software </strong>is developed by deep research in examination domain with highly qualified team which are specialized in creating best efficacy in <strong>online exam preparation software </strong>which is highly efficient in terms of reliability and speed.</p>
+					<p><strong>MERITEST Exam software </strong>is developed by deep research in examination domain with highly qualified team which are specialized in creating best efficacy in <strong>online exam preparation software </strong>which is highly efficient in terms of reliability and speed.</p>
 
-				<p><strong>Final Word:</strong></p>
+					<p><strong>Final Word:</strong></p>
 
-				<p><strong>Online examination system </strong>is gateway towards modernization of conventional examination system. It will decrease your expenses and increase benefits. <strong>MERITEST</strong> always attempts to offer the best <strong>Online Examination Builder </strong>to you.</p>
+					<p><strong>Online examination system </strong>is gateway towards modernization of conventional examination system. It will decrease your expenses and increase benefits. <strong>MERITEST</strong> always attempts to offer the best <strong>Online Examination Builder </strong>to you.</p>
 
-				<p>We are pursuing to sustain and build on the remarkable success which we have enjoyed since <strong>MERITEST online exam tool </strong>launch. Our vision and focus then and now is to challenge the status of working proactively with our customers to find the optimum solutions and applications to assure &lsquo;sustainable&rsquo; long term relationships. These are exciting times for <strong>MERITEST </strong>and we think you will get a sense of our passion, expertise, reliability, and innovative nature as you browse through this website. <strong>We believe in &quot;Make a customer, not a sale&quot;.</strong></p>
-				
-			</div>		
+					<p>We are pursuing to sustain and build on the remarkable success which we have enjoyed since <strong>MERITEST online exam tool </strong>launch. Our vision and focus then and now is to challenge the status of working proactively with our customers to find the optimum solutions and applications to assure &lsquo;sustainable&rsquo; long term relationships. These are exciting times for <strong>MERITEST </strong>and we think you will get a sense of our passion, expertise, reliability, and innovative nature as you browse through this website. <strong>We believe in &quot;Make a customer, not a sale&quot;.</strong></p>
+					
+				</div>
+			</div>	
 		</div>
 		
 		<div class="pt-80 pb-80 bg-light">
@@ -241,5 +237,9 @@ export default {
 
 .image img {
 	filter: grayscale(100%);
+}
+
+.org-color {
+	color: #ff991f;
 }
 </style>
